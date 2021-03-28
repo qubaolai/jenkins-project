023100f0fe7472d7fd341dde67d2ebf85152893f
qubl <qubl@sino-bridge.com>
2021-03-28 18:08:15 +0800

    提交文件

---
+++ hong-xing/src/main/java/com/almond/hongxing/HongXingApplication.java
@@ -0,0 +1,13 @@
+package com.almond.hongxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongXingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongXingApplication.class, args);
+    }
+
+}
---
+++ hong-xing/src/test/java/com/almond/hongxing/HongXingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almond.hongxing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongXingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
